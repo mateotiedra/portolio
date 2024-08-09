5a1ac967f8201567bf3128c514e0800feda38180
Tiedra Mateo <tiedra.mateo@lausanne.ch>
2024-08-09 12:50:55 +0200

    add h2, h3, .. to them + sticky animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import sharkImage from '../../assets/images/landing-shark-white.png'
 import { RiArrowDownDoubleFill } from "react-icons/ri";
 
 function TitleSection() {
+  const downIndicatorRef = useRef(null);
   const titleTextClassName = 'absolute h-0 w-0 leading-[0px] flex justify-start text-[55vw] uppercase font-bold rotate-90 top-0 left-0'
 
+  const [indicatorOpacity, setIndicatorOpacity] = useState(1)
+  const [stickyJump, setStickyJump] = useState(true)
+  const onScroll = useCallback(() => {
+    if (!downIndicatorRef.current) return;
+
+    const containerElemRect = downIndicatorRef.current.getBoundingClientRect()
+    const stickyElemRect = downIndicatorRef.current.children[0].getBoundingClientRect()
+
+    setStickyJump(false);
+    setIndicatorOpacity(1 - (stickyElemRect.top - containerElemRect.top) / (containerElemRect.height - stickyElemRect.height));
+  })
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    }
+  }, [])
+  //console.log(indicatorOpacity);
+
   return (
-    <div className='relative flex justify-between h-[100vh] w-[100vw]'>
+    <div className='relative flex justify-between w-[100vw] h-[265vw]'>
       <div className='absolute flex justify-center w-[100%] pt-[24vw] z-50'>
         <img className='relative rotate-180 w-[90vw]' src={sharkImage} alt='White hammer shark' />
       </div>
@@ -16,9 +37,7 @@ function TitleSection() {
           <h1 className={titleTextClassName + ' items-end'}>
             Mateo
           </h1>
-          <div className='absolute translate-x-[-65%]' style={{ top: 'max(187vw, 90vh)' }}>
-            <RiArrowDownDoubleFill className='animate-bounce w-[13vw] h-[13vw]' />
-          </div>
+
         </div>
         <div className='relative top-[76vw] right-[16vw]'>
           <h1 className={titleTextClassName}>
@@ -26,32 +45,33 @@ function TitleSection() {
           </h1>
         </div>
       </div>
-
+      <div ref={downIndicatorRef} className='absolute left-[21vw] bottom-0 h-[84vw]'>
+        <div className='sticky top-[90vh]' style={{ opacity: indicatorOpacity }}>
+          <RiArrowDownDoubleFill className={'w-[13vw] h-[13vw]'} style={{ animation: stickyJump ? ' bounce 1.5s infinite' : '' }} />
+        </div>
+      </div>
     </div>
   );
 }
 
-/* function AboutMe() {
+function AboutMe() {
   return (
-    <div>
-      <p>About Me</p>
-      <Typography variant='h2'>
-        Developer trying to create nice things out of nothing
-      </Typography>
-      <Typography variant='body1'>
-        Self-taught programmer for more than 10 years, now studying computer
-        science at EPFL. <br />
-        Always looking for new opportunities to create something new.
-      </Typography>
+    <div className='p-section-container'>
+      <h4>About me</h4>
+      <h2>
+        Developer, at least at the beginning
+      </h2>
+      <p>I love to imagine and create things. I love to think about ideas and share them. I love to develop solutions and fight to make them work.</p>
+      <p>That’s why I’ve been a self-taught developer for more than 10 years, and why I am currently studying computer science at EPFL. Programming has always been a great tool to be able to bring ideas to life.</p>
     </div>
   );
-} */
+}
 
 function Home() {
   return (
     <>
       <TitleSection />
-      {/* <AboutMe /> */}
+      <AboutMe />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+  transparent: 'transparent',
+  black: '#000',
+  white: '#fff',
+  gray: {
+    350: '#C3C3C3'
+  }
+  // ...
+}
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}'
@@ -7,8 +16,30 @@ module.exports = {
     extend: {
       fontFamily: {
         kanit: ['Kanit', 'sans-serif'],
-        pacifico: ['Pacifico', 'sans-serif'],
+        pacifico: ['Pacifico', 'serif'],
+        breeserif: ['Bree Serif', 'serif'],
       },
+      fontSize: {
+        base: '16px', // Default font size
+        h1: '2.25rem', // Equivalent to text-4xl
+        h2: '1.875rem', // Equivalent to text-3xl
+        h3: '1.5rem', // Equivalent to text-2xl
+        h4: '1.25rem', // Equivalent to text-xl
+        h5: '1.125rem', // Equivalent to text-lg
+        h6: '1rem', // Equivalent to text-base
+      },
+      lineHeight: {
+        heading: '1', // Default line height
+        base: '1.3', // Default line height
+      },
+      textColor: {
+        base: colors.gray['350'], // Default text color
+        heading: 'white'
+      },
+      color: colors,
+      padding: {
+        'section-container': '2rem'
+      }
     },
   },
   plugins: [],

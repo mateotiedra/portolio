c4e3eca149db96a4cccc6883912aae0f651f0bd1
mateotiedra <mateo.tiedra@outlook.com>
2024-10-10 01:36:29 +0200

    add lumm x 2221

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -19,13 +19,33 @@ export type ProjectProps = {
 
 export const projects: ProjectProps[] = [
     {
+        title: 'Lümm x 2221',
+        id: 'lummx2221',
+        subtitle: 'DJ sets & Panneaux leds',
+        lilTags: {
+            'Date': '28.09.24',
+            'DJ': '6',
+            'Participants': '500',
+            'Staff': '20',
+            'Bière': 'CHF3',
+        },
+        techTags: [<StaffChip />, <AccountingChip />],
+        preview: ['videos/lummx2221.mp4'],
+        color: 'silver',
+        description: <p>
+            Quatrième édition de la <a href='https://www.lumm.love/' target='_blank' rel="noreferrer">LÜMM</a> organisée en collaboration direct avec <a href='https://2221.ch/' target='_blank' rel="noreferrer">2221</a>.
+            Soutenue par la brasserie <a href='https://lanebuleuse.ch/' target='_blank' rel="noreferrer">La Nébuleuse</a> et < a href='https://www.eltonymate.com/' target='_blank' rel="noreferrer" > El Tony Mate</a ></p>,
+        status: 'Gros banger',
+        instaUrl: 'https://www.instagram.com/lumm.love/',
+    },
+    {
         title: 'Artiphys',
         id: 'artiphys-website',
         subtitle: 'Webmaster à',
         lilTags: {
             'Events': '4',
+            'Festivalier.x.s': '1200',
             'Membres du comité': '29',
-
         },
         techTags: [<ReactChip />, <TailwindChip />, <GoogleAdminChip />],
         link: 'https://artiphys.ch/',
@@ -34,7 +54,7 @@ export const projects: ProjectProps[] = [
         description: <p>
             Membre du comité de l'association <a href='https://www.artiphys.ch/' target='_blank' rel="noreferrer">Artiphys</a> pour cette éditon 2025. J'ai refait le site web pour le faire correspondre au nouveau comité et à la nouvelle direction artistique. Je gère également toute la partie gestion du Google Workspace partagé par les membres du comité.</p>,
         status: 'Membre du comité',
-        instaUrl: 'https://www.instagram.com/okalo.ch/',
+        instaUrl: 'https://www.instagram.com/artiphysfestival/',
         since: 'Juin 2024'
     },
     {
@@ -54,7 +74,7 @@ export const projects: ProjectProps[] = [
             Utilisé dans plus d'une dizaine de collèges genevois, <a href='https://www.okalo.ch/' target='_blank' rel="noreferrer">Okalo</a> facilite
             l'achat et la vente de livres d'occasion pour les étudiants, leur permettant ainsi de faire des économies et de donner une seconde vie à leurs bouquins inutilisés.
         </p>,
-        status: 'actif',
+        status: 'Actif',
         instaUrl: 'https://www.instagram.com/okalo.ch/',
         since: '2022'
     },
@@ -63,7 +83,7 @@ export const projects: ProjectProps[] = [
         id: 'lummc',
         subtitle: 'Défilé, concerts & DJ sets',
         lilTags: {
-            'Date': '11.05.22',
+            'Date': '11.05.24',
             'Participants': '700',
             'Staffs': '46',
             'Bière': 'CHF2'
@@ -78,9 +98,8 @@ export const projects: ProjectProps[] = [
             Avec la participation du bar <a href='https://satellite.bar/' target='_blank' rel="noreferrer">Satellite</a> et <a href='https://artepoly.agepoly.ch/' target='_blank' rel="noreferrer">ArtePoly</a> de l'EPFL.
             Soutenue par la brasserie <a href='https://lanebuleuse.ch/' target='_blank' rel="noreferrer">La Nébuleuse</a> et <a href='https://www.eltonymate.com/' target='_blank' rel="noreferrer">El Tony Mate</a>
         </p>,
-        status: 'terminé',
+        status: 'Big succès',
         instaUrl: 'https://www.instagram.com/lumm.love/',
-        since: 'mai 2024'
     },
     {
         title: 'Morii',
@@ -99,7 +118,7 @@ export const projects: ProjectProps[] = [
             <a href='https://morii.lumm.love/' target='_blank' rel="noreferrer">Morii</a> est
             une plateforme de partage de photos qui permet de se souvenir des bons moments grâce à des photos triées et sans perte de qualité.
         </p>,
-        status: 'en pause',
+        status: 'En pause',
         since: 'mai 2024'
     },
     {
@@ -116,7 +135,7 @@ export const projects: ProjectProps[] = [
         description: <p>
             Jeu mobile addictif développé pendant le covid. J'ai été en contact avec un représentant de <a href='https://lionstudios.cc/' target='_blank' rel="noreferrer">LionStudios</a> qui s'était montré intéressé par la publication du jeu. Cependant, après une phase de test, les retours des joueurs n'ont pas été suffisamment positifs, et le projet n'a pas été poursuivi.
         </p>,
-        status: 'échec  ',
+        status: 'Échec  ',
         since: '2020'
     },
 ];

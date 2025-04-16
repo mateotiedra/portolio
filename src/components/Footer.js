2a56cb8bc347640d8a99835835f461f1f571cf27
mateotiedra <mateo.tiedra@outlook.com>
2025-04-16 13:50:18 +0200

    add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
-import { FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa6';
+import { FaInstagram, FaLinkedin, FaEnvelope, FaGithub } from 'react-icons/fa6';
 
 function Footer() {
   return (
     <div className='w-full flex flex-row justify-center gap-8 py-16'>
+      <a href='https://github.com/mateotiedra'>
+        <FaGithub className='text-white text-2xl' />
+      </a>
       <a href='https://www.linkedin.com/in/mateo-tiedra-405614282'>
         <FaLinkedin className='text-white text-2xl' />
       </a>
